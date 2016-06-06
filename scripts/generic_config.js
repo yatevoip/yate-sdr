63f57c4672ca846c328f9c3a72965c35820a51e6
monica <monica@42869567-5b34-0510-a752-ce2ba1beb872>
2016-06-06 14:18:03 +0000

    Added generic functions to work with configuration files. Added basic implementation for setting ybladerf.conf and satsite.conf
    git-svn-id: http://voip.null.ro/svn/yate-sdr@2 42869567-5b34-0510-a752-ce2ba1beb872

---
+++ scripts/generic_config.js
@@ -0,0 +1,245 @@
+/**
+ * generic_config.js
+ * This file is part of the YATE Project http://YATE.null.ro
+ *
+ * Yet Another Telephony Engine - a fully featured software PBX and IVR
+ * Copyright (C) 2014-2015 Null Team
+ *
+ * This software is distributed under multiple licenses;
+ * see the COPYING file in the main directory for licensing
+ * information for this specific distribution.
+ *
+ * This use of this software may be subject to additional restrictions.
+ * See the LEGAL file in the main directory for details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+function GenericConfig(file, section_names, overwrite)
+{
+    if (overwrite == undefined)
+	overwrite = true;
+
+    this.error = new Object;
+
+    if (file != undefined)
+	this.file = file;
+    if (section_names != undefined)
+	this.sections = section_names;
+
+    this.overwrite = overwrite;
+}
+
+GenericConfig.prototype = new Object;
+
+// Load and clear config file if overwrite was true
+GenericConfig.prototype.prepareConfig = function(params)
+{
+    this.conf = prepareConf(this.file,true);
+
+    // clear ybts sections
+    var sections = this.conf.sections();
+    for (var sect of sections) {
+	if (this.overwrite)
+	    this.conf.clearSection(sect);
+	else {
+	    this.current_config = this.getConfig();
+	}
+    }
+};
+
+// Call validations and set configurations
+GenericConfig.prototype.setConfig = function()
+{
+    var section_name, section, section_params, param_name, param_value;
+
+    if (!this.overwrite) {
+	// keep existing section and values
+	for (section_name in this.current_config) {
+	    section = this.conf.getSection(section_name,true);
+	    section_params = this.current_config[section_name];
+	    for (param_name in section_params) 
+		section.setValue(param_name, section_params[param_name]);
+	}
+	// received configurations will be put over this ones
+    }
+
+    //return error if required section is missing
+    for (section_name_req in this.params_required) {
+	if (!params[section_name_req]) {
+	    this.error.reason = "Required section: '"+ section_name_req +"' is missing.";
+	    this.error.error = 402;
+	    return false;
+	}
+    }
+
+    for (var i = 0; i < this.sections.length; i++) {
+	section_name = this.sections[i];
+
+	section_params = params[section_name];
+	if (!section_params)
+	    continue;
+
+	section = this.conf.getSection(section_name,true);
+	for (param_name in section_params) {
+	    param_value = section_params[param_name];
+	    if (!this.validateConfig(section_name, param_name, param_value)) 
+		return false;
+
+	    section.setValue(param_name, param_value);
+	}
+    }
+    return true;
+};
+
+// Validate new configurations
+GenericConfig.prototype.validateConfig = function(section_name, param_name, param_value)
+{
+    var validations = this.validations[section_name][param_name];
+
+    var required = this.params_required[section_name];
+    if (required!=undefined) {
+	for (var i=0; i<required.length; i++) {
+	    var param_desc = "required: " + required[i];
+	    if (isParamMissing(this.error, param_desc, params.gsm[required[i]]))
+		return false;
+	}
+    }
+
+    if (param_value === null || param_value == "") {
+	if (!inArray(param_name, this.params_allowed_empty)) {
+	    this.error.reason = "Field "+param_name+" from "+ section_name +" can't be empty."; 
+	    this.error.error = 402;
+	    return false;
+	}
+    }
+    if (validations["minimum"] != undefined)
+	if (!checkFieldValidity(this.error, section_name, param_name, param_value, validations["minimum"], validations["maximum"]))
+	    return false;
+
+    if (validations["regex"] != undefined)
+	if (!checkFieldValidity(this.error, section_name, param_name, param_value, undefined, undefined, validations["regex"]))
+	    return false;
+
+    if (validations["select"] != undefined)
+	if (!checkValidSelect(this.error, param_name, param_value, validations["select"], section_name))
+	    return false;
+
+    if (validations["callback"] != undefined) {
+	var callback = validations["callback"];
+	if ("object" == typeof callback) {
+	    if (!callback(this.error, param_name, param_value, section_name))
+		return false;
+	}
+    }
+
+    return true;
+};
+
+// Save config files
+GenericConfig.prototype.saveConfig = function()
+{
+    if (!checkUnlocked(this.error,[this.conf]))
+	return false;
+    if (!saveConf(this.error,this.conf))
+	return false;
+    return true;
+};
+
+// Get configuration from file
+GenericConfig.prototype.getConfig = function()
+{
+    var c, sections, keys, key, section, res;
+
+    c = new ConfigFile(Engine.configFile(this.file));
+    c.load("Could not load "+this.name);
+    sections = c.sections();
+
+    res = {};
+    for (var section_name in sections) {
+	res[section_name] = {};
+	section = sections[section_name];
+	keys = section.keys();
+	for (key of keys) {
+	    res[section_name][key] = section.getValue(key);
+	}
+    }
+
+    return res;
+};
+
+API.on_get_generic_file = function(configObj,params)
+{
+    if (debug)
+	dumpObj("configObj",configObj);
+    return configObj.getConfig();
+};
+
+// Generoc function to handle set requests
+API.on_set_generic_file = function(configObj,params,msg,setNode)
+{
+    if (debug)
+	dumpObj("configObj",configObj);
+
+    if (setNode && !params) {
+	configObj.prepareConfig(msg);
+	if (!configObj.saveConfig())
+	    return configObj.error;
+	return { name: configObj.name };
+    }
+
+    if (!checkJson(configObj.error,params,msg.json))
+	return configObj.error;
+
+    configObj.prepareConfig(msg);
+
+    if (!configObj.setConfig(msg))
+	return configObj.error;
+
+    // Save config
+    if (!configObj.saveConfig())
+	return configObj.error;
+
+    Engine.debug(Engine.DebugAll, "Saved config");
+    return { name: configObj.name };
+};
+
+// Prepare a config file:
+// Load, clear, set updated info
+function prepareConf(name,msg,clear)
+{
+    var c = new ConfigFile(Engine.configFile(name));
+    var l = c.getBoolValue("general","locked");
+    if (false !== clear)
+	c.clearSection();
+    c.setValue("general","updated",msg.received);
+    c.setValue("general","locked",l);
+    return c;
+};
+
+function saveConf(error,conf)
+{
+    if (conf.getBoolValue("general","locked"))
+	return setStorageError(error,"Locked config file '" + conf.name() + "'",false);
+    if (conf.save())
+	return true;
+    return setStorageError(error,"Failed to save config file '" + conf.name() + "'",false);
+}
+
+function checkUnlocked(error,confs)
+{
+    for (var c of confs) {
+	if (c.getBoolValue("general","locked"))
+	    return setStorageError(this.error,"One or more of the config files is locked",false);
+    }
+    return true;
+}
+
+function dumpObj(prefix,obj)
+{
+    var dump = Engine.dump_r(obj);
+    Engine.output(prefix + ":\r\n-----\r\n" + dump + "\r\n-----");
+}
+/* vi: set ts=8 sw=4 sts=4 noet: */
---
+++ scripts/sdr_config.js
@@ -0,0 +1,193 @@
+/**
+ * sdr_config.js
+ * This file is part of the YATE Project http://YATE.null.ro
+ *
+ * Yet Another Telephony Engine - a fully featured software PBX and IVR
+ * Copyright (C) 2014-2015 Null Team
+ *
+ * This software is distributed under multiple licenses;
+ * see the COPYING file in the main directory for licensing
+ * information for this specific distribution.
+ *
+ * This use of this software may be subject to additional restrictions.
+ * See the LEGAL file in the main directory for details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+//#pragma cache "true"
+//#pragma compile "sdr_config.jsc"
+//#pragma trace "cachegrind.out.sdr_config"
+
+#require "generic_config.js"
+
+//  Satsite file configuration object
+function SatSiteConfig()
+{
+    GenericConfig.apply(this);
+    this.file = "satsite";
+    this.sections = ["basic", "site-equipment", "shutdown"];
+    this.overwrite = false;
+};
+SatSiteConfig.prototype = GenericConfig.prototype;
+
+// Get configurations from satsite.conf
+API.on_get_satsite_node = function(params,msg)
+{
+    var satsite = new SatSiteConfig;
+    return API.on_get_generic_file(satsite,msg);
+};
+
+// Configure satsite.conf related parameters
+API.on_set_satsite_node = function(params,msg,setNode)
+{
+    var satsite = new SatSiteConfig;
+    return API.on_set_generic_file(satsite,params,msg,setNode);
+};
+
+// Ybladerf file configuration object
+function YBladeRfConfig()
+{
+    GenericConfig.apply(this);
+    this.file = "ybladerf";
+    this.sections = ["general", "libusb", "filedump", "test_name"];
+    this.overwrite = false;
+}
+YBladeRfConfig.prototype = GenericConfig.prototype;
+
+// Get configurations from ybladerfconf
+API.on_get_ybladerf_node = function(params,msg)
+{
+    var ybladerf = new YBladeRfConfig;
+    return API.on_get_generic_file(ybladerf,msg);
+};
+
+// Configure ybladerf.conf related parameters
+API.on_set_ybladerf_node = function(params,msg,setNode)
+{
+    var ybladerf = new YBladeRfConfig;
+    return API.on_set_generic_file(ybladerf,params,msg,setNode);
+};
+
+function checkJson(error,params,json)
+{
+    if (params)
+    	return true;
+    if (json) {
+    	error.reason = "Invalid JSON content.";
+    	error.error = 401;
+    }
+    else {
+    	error.reason = "Missing all parameters.";
+    	error.error = 402;
+    }
+    return false;
+}
+
+// Implement basic get request
+API.on_get_node = function(params,msg)
+{
+    var func, have_settings, error;
+
+    json = {};
+    for (var conf of confs) {
+    	func = API["on_get_"+conf+"_node"];
+	if ("function" != typeof func.apply) 
+    	    continue;
+
+    	json[conf] = func(params);
+    	have_settings = true;
+    }
+    return { name: "node", object: json };
+};
+
+// Configure node params
+API.on_set_node = function(params,msg)
+{
+    if (debug)
+	dumpObj("on_set_node (params)",msg.json);
+
+    var error = new Object;
+    if (!checkJson(error,params,msg.json))
+	return error;
+
+    var confParams, func, error;
+    var have_settings = false;
+
+    for (var conf of confs) {
+	if (debug)
+	    Engine.debug(Engine.DebugAll, "Searching config params for file "+conf);
+	confParams = params[conf];
+	if (!confParams)
+	    continue;
+	func = API["on_set_"+conf+"_node"];
+	if ("function" != typeof func.apply) {
+	    var mess_reason = "Missing support for configuring "+conf;
+	    return { reason: mess_reason, error: 201};
+	}
+	error = func(confParams,msg,true);
+	if (error.reason) {
+	    error.reason = conf + ": " + error.reason;
+	    return error;
+	}
+	have_settings = true;
+    }
+
+    if (!have_settings)
+	return { reason: "Missing configuration parameters.", error: 402 };
+
+    if (parseBool(params.restart)) {
+	Engine.output("Equipment restart on node config: " + msg.received);
+	Engine.restart();
+    }
+    return { name: "node", object: conf_node};
+};
+
+// Callback used when handler for api.request message is installed
+function onApiRequest(msg)
+{
+    var func = API["on_" + msg.operation];
+    if ("function" != typeof func.apply)
+	return false;
+    var res = func(JSON.parse(msg.json),msg);
+    if (!res)
+	return false;
+    if ("" != res.reason) {
+	msg.retValue("-");
+	msg.error = res.error;
+	msg.reason = res.reason;
+	if (debug)
+	    Engine.debug(Engine.DebugNote,
+		msg.operation + " failed: " + res.error + " '" + res.reason + "'");
+    }
+    else {
+	msg.retValue(res.name);
+	msg.json = JSON.stringify(res.object);
+	if (debug)
+	    Engine.debug(Engine.DebugInfo,msg.operation + " succeeded");
+    }
+    return true;
+}
+
+// Callback used when handler for engine.init message is installed
+function onReload(msg)
+{
+    if (msg.plugin && ("api_config" != msg.plugin))
+	return false;
+    loadCfg();
+    return !!msg.plugin;
+}
+
+function loadCfg(first)
+{
+    config.load(true);
+    if (first) {
+	var tmp = config.getValue("debug");
+	if ("" != tmp)
+	    Engine.setDebug(tmp);
+	debug = Engine.debugEnabled();
+    }
+}
+/* vi: set ts=8 sw=4 sts=4 noet: */
