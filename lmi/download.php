d1ff5b011495850b2cfcb4fa134bf7b7b6f6a4db
dana <dana@42869567-5b34-0510-a752-ce2ba1beb872>
2016-08-17 16:07:59 +0000

    Used $upload_path for downloading files.
    git-svn-id: http://voip.null.ro/svn/yate-sdr@40 42869567-5b34-0510-a752-ce2ba1beb872

--- lmi/download.php
+++ lmi/download.php
@@ -1,15 +1,15 @@
 <?php
 require_once("ansql/lib.php");
 require_once("defaults.php");
-global $pysim_csv,$yate_conf_dir;
+global $pysim_csv, $upload_path;
 
 if (getparam("file")) {
 	$filename = getparam("file");
-	$path_file = $yate_conf_dir .$filename;
+	$path_file = $upload_path .$filename;
 	$fp = @fopen($path_file,'rb');
 } else {
 	$fp = @fopen($pysim_csv, 'rb');
-	$filename = str_replace($yate_conf_dir, "", $pysim_csv);
+	$filename = str_replace($upload_path, "", $pysim_csv);
 }
 header('Content-Type: "application/octet-stream"');
 header('Content-Disposition: attachment; filename="'.$filename.'"');
@@ -17,7 +17,7 @@ header("Content-Transfer-Encoding: binary");
 header('Expires: 0');
 header('Pragma: no-cache');
 if (getparam("file"))
-	header("Content-Length: ".filesize($yate_conf_dir .getparam("file")));
+	header("Content-Length: ".filesize($upload_path .getparam("file")));
 else
 	header("Content-Length: ".filesize($pysim_csv));
 
