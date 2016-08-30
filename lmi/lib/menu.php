79bd5716986bfa90d2f5aaa5d5fac3baf01e5a0b
dana <dana@42869567-5b34-0510-a752-ce2ba1beb872>
2016-08-30 14:08:31 +0000

    Removed footer note.
    git-svn-id: http://voip.null.ro/svn/yate-sdr@53 42869567-5b34-0510-a752-ce2ba1beb872

--- lmi/lib/menu.php
+++ lmi/lib/menu.php
@@ -129,9 +129,9 @@ function get_content()
 			</td>
 		</tr>
 	</table>
-	<div class="footer_note">
+<!--	<div class="footer_note">
 		Note! To disable nib mode and enable roaming mode see <a class='llink' href='http://wiki.yatebts.com/index.php/Javascript_Roaming'>Javascript Roaming</a>
-	</div>
+	</div>-->
 <?php
 }
 
