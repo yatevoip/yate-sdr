a6989cfea4b7ecde383b66fd9e92cb34872aedcc
monica <monica@42869567-5b34-0510-a752-ce2ba1beb872>
2017-03-09 15:24:18 +0000

    Fix: clean session information when entering index.php
    git-svn-id: http://voip.null.ro/svn/yate-sdr@79 42869567-5b34-0510-a752-ce2ba1beb872

--- lmi/index.php
+++ lmi/index.php
@@ -3,7 +3,7 @@
  * index.php
  * This file is part of the Yate-BTS Project http://www.yatebts.com
  *
- * Copyright (C) 2014 Null Team
+ * Copyright (C) 2016 - 2017 Null Team
  *
  * This software is distributed under multiple licenses;
  * see the COPYING file in the main directory for licensing
@@ -16,6 +16,10 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
+
+// if user enters in index.php clean session info and redirect to main.php
+session_start();
+session_unset();
 
 Header("Location: main.php");
 ?>
