9919a0411caf3798e74db3eaf84c738d03eb5068
monica <monica@42869567-5b34-0510-a752-ce2ba1beb872>
2017-03-24 15:50:18 +0000

    Removed config.php that should not have been submitted.
    git-svn-id: http://voip.null.ro/svn/yate-sdr@108 42869567-5b34-0510-a752-ce2ba1beb872

--- lmi/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server_name = "192.168.168.208";
-$yate_ip = "192.168.168.208";
-
-$max_xdebug_mess = 250;
-
-$debug_notify = array("mail" => array("monica@null.ro","dana@null.ro"));
-$enable_debug_buttons = true;
-?>
