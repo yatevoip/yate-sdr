76770ee7995d204bf00c5855c1c26718f6cd3c9e
dana <dana@42869567-5b34-0510-a752-ce2ba1beb872>
2017-03-29 11:08:46 +0000

    Removed save_page_info().
    git-svn-id: http://voip.null.ro/svn/yate-sdr@116 42869567-5b34-0510-a752-ce2ba1beb872

--- lmi/main.php
+++ lmi/main.php
@@ -62,7 +62,6 @@ $_SESSION["level"] = $level;
 
 $_SESSION["main"] = "main.php";
 
-save_page_info();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
