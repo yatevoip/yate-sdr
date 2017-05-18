1023fca01b53ba8dbc0ff234c28a880f10dfe594
dana <dana@42869567-5b34-0510-a752-ce2ba1beb872>
2017-05-18 14:53:09 +0000

    Implemented function to build selected options for the chosen radio band.
    git-svn-id: http://voip.null.ro/svn/yate-sdr@160 42869567-5b34-0510-a752-ce2ba1beb872

--- lmi/pages.php
+++ lmi/pages.php
@@ -18,10 +18,12 @@
  * pages.php
  * Contains functions called using XMLHttpRequest object from javascript.js
  */ 
-require_once("ansql/use_json_requests.php");
-require_once("ansql/lib.php");
-require_once("lib/lib_requests.php");
+require_once "ansql/use_json_requests.php";
+require_once "ansql/lib.php";
+require_once "lib/lib_requests.php";
+require_once "ansql/sdr_config/create_radio_band_select_array.php";
 
+session_start();
 $method = getparam("method");
 
 if (call_user_func($method) === FALSE)
@@ -31,4 +33,19 @@ function node_status_json()
 {
 	$res = node_status();
 	print json_encode($res);
+}
+
+function get_selected_band()
+{
+	$radio_band = getparam("radio_band");
+	$_SESSION["Radio.Band"] = $radio_band;
+	$band_options = prepare_gsm_field_radio_c0();
+	foreach ($band_options as $k=>$option) {
+		$value = $option["Radio.C0_id"];
+		$style = "";
+		if ($value=="__disabled")
+			$style = "disabled=\"disabled\"";
+
+		print '<option '.$style.' value="'.$value.'">'.$option["Radio.C0"].'</option>';
+	}
 }
