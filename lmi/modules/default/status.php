82242868c6fda2ef2e05586b547275a52523ae21
monica <monica@42869567-5b34-0510-a752-ce2ba1beb872>
2017-10-13 13:49:13 +0000

    Added option to retrieve additional details when querying equipment status. Added refresh button on page.
    git-svn-id: http://voip.null.ro/svn/yate-sdr@190 42869567-5b34-0510-a752-ce2ba1beb872

--- lmi/modules/default/status.php
+++ lmi/modules/default/status.php
@@ -2,7 +2,23 @@
 
 function status()
 {
-	$res = make_request(array(),"query_stats");
+	$request_params = array();
+	if (getparam("additional_details")=="on")
+		$request_params["details"] = true;
+
+	$res = make_request($request_params,"query_stats");
+
+	print "<div class='status_option'>";
+	start_form();
+	print "<input type='hidden' name='module' value='status' />";
+	print "Additional details:";
+        print "<input type='checkbox' name='additional_details' ";
+	if (getparam("additional_details")=="on")
+		print " checked";
+	print "/>";
+	print "&nbsp;&nbsp;&nbsp;<input type='submit' value='Refresh' />";
+	end_form();
+	print "</div>";
 
 	if ($res["code"]!="0") {
 		errormess("API:[". $res["code"]."] ".$res["message"], "no");
